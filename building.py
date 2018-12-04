499b48ec743415c3ab11063b749c09ecd5905d91
Pouya Pourbaba <p.pourbaba@gmail.com>
2018-12-05 00:42:11 +0200

    files cleaned for this branch

--- building.py
+++ building.py
@@ -1,8 +1,6 @@
 import numpy as np
 import math
 
-from intersection import isect_line_plane_v3
-
 class Building():
     def __init__(self, x1, y1, h, w):
         self.x1 = x1 - 500
@@ -11,78 +9,3 @@ class Building():
         self.x2 = x1 + w - 500
         self.y2 = y1 + w - 500
         self.h = h
-
-    def plane1(self, x1, y1, h, w):
-        x = x1
-        y = y1
-        p1 = np.array([x,   y,   0])
-        p2 = np.array([x+w, y,   0])
-        p3 = np.array([x,   y,   h])
-        n = np.cross(p2 - p1, p3 - p1)
-        # normalizing the vector n
-        abs_n = math.sqrt(np.inner(n,n))
-        for i in range(3):
-            if n[i] != 0:
-                n[i] = n[i]/abs_n
-
-        return p1, n
-
-    def plane2(self, x1, y1, h, w):
-        x = x1
-        y = y1
-        p1 = np.array([x+w, y,   0])
-        p2 = np.array([x+w, y+w, 0])
-        p3 = np.array([x+w, y,   h])
-        n = np.cross(p2 - p1, p3 - p1)
-        # normalizing the vector n
-        abs_n = math.sqrt(np.inner(n, n))
-        for i in range(3):
-            if n[i] != 0:
-                n[i] = n[i] / abs_n
-
-        return p1, n
-
-    def plane3(self, x1, y1, h, w):
-        x = x1
-        y = y1
-        p1 = np.array([x+w, y+w, 0])
-        p2 = np.array([x,   y+w, 0])
-        p3 = np.array([x+w, y+w, h])
-        n = np.cross(p2 - p1, p3 - p1)
-        # normalizing the vector n
-        abs_n = math.sqrt(np.inner(n, n))
-        for i in range(3):
-            if n[i] != 0:
-                n[i] = n[i] / abs_n
-
-        return p1, n
-
-    def plane4(self, x1, y1, h, w):
-        x = x1
-        y = y1
-        p1 = np.array([x,  y,   0])
-        p2 = np.array([x,  y+w, 0])
-        p3 = np.array([x,  y,   h])
-        n = np.cross(p2-p1, p3-p1)
-        # normalizing the vector n
-        abs_n = math.sqrt(np.inner(n, n))
-        for i in range(3):
-            if n[i] != 0:
-                n[i] = n[i] / abs_n
-
-        return p1, n
-
-    def plane5(self, x1, y1, h, w):
-        x = x1
-        y = y1
-        p1 = np.array([x,   y,   h])
-        p2 = np.array([x,   y+w, h])
-        p3 = np.array([x+w, y,   h])
-        n = np.cross(p2 - p1, p3 - p1)
-        # normalizing the vector n
-        abs_n = math.sqrt(np.inner(n, n))
-        for i in range(3):
-            if n[i] != 0:
-                n[i] = n[i] / abs_n
-
-        return p1, n
--- env.py
+++ env.py
@@ -3,7 +3,6 @@ import numpy as np
 import math
 
 # my own scripts
-from intersection import isect_line_plane_v3
 import uav_location
 
 # 3d environment figure libs
@@ -51,34 +50,6 @@ print("(x1, y1):", "(", buildings[1].x1, ",", buildings[1].y1, ")")
 print("(x2, y2):", "(", buildings[1].x2, ",", buildings[1].y1, ")")
 print("h:", buildings[1].h)
 
-# check the intersection
-p1, n1 = buildings[0].plane1(buildings[0].x1, buildings[0].y1, buildings[0].h, buildings[0].w)
-#print(p1)
-print(n1)
-
-
-p2, n2 = buildings[0].plane2(buildings[0].x1, buildings[0].y1, buildings[0].h, buildings[0].w)
-print(n2)
-
-print(buildings[1].x1, buildings[1].x2)
-print(buildings[1].y1, buildings[1].y2)
-
-
-#Right now, I have prepared the functions inside the Building class to calculate the normal functions of 5 main surfaces of the buildings (not the bottom surface).
-
-# I will use the normal vectors and the point on the plane (p1 and n from the plane1,...,plane5 function in the Building class) in the isect_line_plane_v3 function
-# to find out if the link from the UAV intercepts with any of the surfaces of the buildings. If no intercept is recognized the link is LoS, otherwise, the link is
-# a NLoS link.
-
-# arbitrary vehicle location
-v1 = [-20, 250, 0]
-
-# find out if the vector returned by the function "isect_line_plane_v3" is the point of intersection
-ins = isect_line_plane_v3([-225,-300,25], [-225,-200,25], p1, n1)
-if (ins[0]>=buildings[100].x1 and ins[0]<=buildings[100].x2) and (ins[1]>=buildings[100].y1 and ins[1]<=buildings[100].y2):
-    print('intercepted')
-    print(ins)
-
 fig = plt.figure()
 ax1 = fig.add_subplot(111, projection='3d')
 
@@ -95,8 +66,6 @@ dy = np.ones(nOfB) * 50
 dz = h
 
 ax1.bar3d(xpos,ypos,zpos, dx,dy,dz)
-if (ins[0]>=buildings[100].x1 and ins[0]<=buildings[100].x2) and (ins[1]>=buildings[100].y1 and ins[1]<=buildings[100].y2):
-    ax1.scatter(ins[0],ins[1],ins[2])
 ax1.scatter(L['x'], L['y'], L['z'], c='r')
 plt.xlabel('x')
 plt.ylabel('y')
